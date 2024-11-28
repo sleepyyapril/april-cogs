30d9ebf8af8080de4557c461ed101a46250f55ff
sleepyyapril <flyingkarii@gmail.com>
2024-11-28 18:22:13 -0400

    meow

--- ahelp_replies/ahelp_replies.py
+++ ahelp_replies/ahelp_replies.py
@@ -158,12 +158,12 @@ class ahelp_replies(commands.Cog):
         
         cur_server = servers[server_id]
         
+        if message.webhook_id == None or message.author == self.bot.user:
+            return
+        
         if message.channel.type == ChannelType.public_thread:
             print("using threads")
             return await self.handle_thread(message, message.channel.starter_message, cur_server)
-        
-        if message.webhook_id == None or message.author == self.bot.user:
-            return
 
         if channel_to_use.type != ChannelType.text:
             return
