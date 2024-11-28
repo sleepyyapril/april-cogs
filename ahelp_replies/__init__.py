4e30aadcf0520c026a274f9a5de660a91d47d32b
sleepyyapril <flyingkarii@gmail.com>
2024-11-28 05:08:08 -0400

    mrow

--- ahelp_replies/__init__.py
+++ ahelp_replies/__init__.py
@@ -1,4 +1,4 @@
-from .ahelp_replies import AHelpReplies
+from .ahelp_replies import ahelp_replies
 
 async def setup(bot):
-    await bot.add_cog(AHelpReplies(bot))
+    await bot.add_cog(ahelp_replies(bot))
