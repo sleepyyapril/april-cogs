1f8e1ee3dbcdcb7786f85f7d8fc6c3d748bd5571
sleepyyapril <flyingkarii@gmail.com>
2024-11-28 19:19:17 -0400

    hmph

--- ahelp_replies/ahelp_replies.py
+++ ahelp_replies/ahelp_replies.py
@@ -67,7 +67,7 @@ async def send_reply(session: aiohttp.ClientSession, server, username: str) -> t
         async with session.post(
                 f'http://{server["server_ip"]}/admin/actions/send_bwoink',
                 auth=aiohttp.BasicAuth("SS14Token", server["token"]),
-                data=b'{"Guid": "' + userId.encode() + '", "Text": "(DC) [color=lightblue]Name:[/color] Test", "useronly": false }'
+                data='{"Guid": "' + userId + '", "Text": "(DC) [color=lightblue]Name:[/color] Test", "useronly": false }'.encode()
             ) as resp:
             return resp.status, await resp.text()
 
